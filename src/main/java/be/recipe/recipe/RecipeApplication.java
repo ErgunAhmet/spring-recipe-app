776f8bc0d3a0337c15f6cf6afd7801f2dc221ed7
ErgunAhmet <AhmetErgun2000@protonmail.com>
2021-03-24 21:51:14 +0100

    creating recipe, Ingredient and Notes entities with relationships

--- src/main/java/be/recipe/recipe/RecipeApplication.java
+++ src/main/java/be/recipe/recipe/RecipeApplication.java
@@ -8,7 +8,7 @@ public class RecipeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RecipeApplication.class, args);
-        System.out.println("test1");
+        System.out.println("hello");
     }
 
 }
