29e0c278a23203b4b309ae0c0dea351ab9815745
ErgunAhmet <AhmetErgun2000@protonmail.com>
2021-03-25 21:42:40 +0100

    added two recipes with bootstrap

--- src/main/java/be/recipe/recipe/domain/Ingredient.java
+++ src/main/java/be/recipe/recipe/domain/Ingredient.java
@@ -8,13 +8,25 @@ public class Ingredient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Lob
     private String description;
     private BigDecimal amount;
     @ManyToOne
     private Recipe recipe;
 
-    @OneToOne(fetch =FetchType.EAGER, cascade = CascadeType.ALL)
+    @OneToOne(fetch =FetchType.EAGER)
     private UnitOfMeasure uom;
+
+    public Ingredient() {
+    }
+
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
+        this.description = description;
+        this.amount = amount;
+        this.uom = uom;
+        this.recipe = recipe;
+    }
+
 
     public Long getId() {
         return id;
